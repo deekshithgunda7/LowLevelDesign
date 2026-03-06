f916396b71418e36523bf8a8750e93552a73bba8
deekshithgunda7 <deekshith.gunda7@gmail.com>
2026-03-06 12:26:50 -0600

    missing number

---
+++ missingNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int sum=0;
+        for(int i=0;i<=nums.length-1;i++){
+               sum=sum+nums[i];
+        }
+
+        return (n * (n+1) / 2)-sum; 
+        
+    }
+}
